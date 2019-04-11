3a3306a61b4b48f7e47f50d760219672e4af8eca
77web <info@77-web.com>
2019-04-11 22:39:16 +0900

    added doctrine/doctrine-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     PHPMentors\WorkflowerBundle\PHPMentorsWorkflowerBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
