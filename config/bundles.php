87b4d7b640512f72215978aacfdba041fa2af466
Hiromi Hishida <info@77-web.com>
2019-04-12 23:01:22 +0900

    Revert "Sample bpmn and classes"

--- config/bundles.php
+++ config/bundles.php
@@ -12,7 +12,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     PHPMentors\WorkflowerBundle\PHPMentorsWorkflowerBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
