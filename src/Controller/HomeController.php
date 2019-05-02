31018a7d590c8c54bdbf446e7a58260f9120df63
77web <info@77-web.com>
2019-05-02 14:18:46 +0900

    deleted unnecessary files from ttskch/symfony-micro-skeleton

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/", name="home_")
@@ -20,24 +16,5 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig');
-    }
-
-    /**
-     * @Route("/form", name="form")
-     */
-    public function form(Request $request, TranslatorInterface $translator)
-    {
-        $form = $this->createForm(ContactType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('success', $translator->trans('Form is submitted.'));
-
-            return $this->redirectToRoute('home_form');
-        }
-
-        return $this->render('home/form.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 }
