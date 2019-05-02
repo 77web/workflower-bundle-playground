eac7c83d412eacb3b09e4adeef7253704880606b
77web <info@77-web.com>
2019-05-02 19:11:33 +0900

    defined participants: Dev, Reviewer

---
+++ src/Participant/Dev.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Participant;
+
+
+use PHPMentors\Workflower\Workflow\Participant\ParticipantInterface;
+use PHPMentors\Workflower\Workflow\Resource\ResourceInterface;
+
+class Dev implements ParticipantInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     */
+    public function __construct()
+    {
+        $this->id = 1;
+        $this->name = 'dummy-dev';
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function hasRole($role)
+    {
+        return $role === 'dev';
+    }
+
+    public function setResource(ResourceInterface $resource)
+    {
+        $this->resource = $resource;
+    }
+
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ src/Participant/Reviewer.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Participant;
+
+
+use PHPMentors\Workflower\Workflow\Participant\ParticipantInterface;
+use PHPMentors\Workflower\Workflow\Resource\ResourceInterface;
+
+class Reviewer implements ParticipantInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     */
+    public function __construct()
+    {
+        $this->id = 1;
+        $this->name = 'dummy-reviewer';
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function hasRole($role)
+    {
+        return $role === 'reviewer';
+    }
+
+    public function setResource(ResourceInterface $resource)
+    {
+        $this->resource = $resource;
+    }
+
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
