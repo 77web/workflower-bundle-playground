292487262de120a0fe74fe07d92347ace8270280
77web <info@77-web.com>
2019-05-06 20:49:09 +0900

    added FixPullRequestUsecase

--- tests/Functional/Usecase/FixPullRequestUsecaseTest.php
+++ tests/Functional/Usecase/FixPullRequestUsecaseTest.php
@@ -6,54 +6,41 @@ namespace App\Tests\Functional\Usecase;
 
 use App\Entity\PullRequest;
 use App\Usecase\CreatePullRequestUsecase;
+use App\Usecase\FixPullRequestUsecase;
 use App\Usecase\ReviewPullRequestUsecase;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
-class ReviewPullRequestUsecaseTest extends WebTestCase
+class FixPullRequestUsecaseTest extends WebTestCase
 {
     public function setUp()
     {
         $this->loadFixtureFiles([]);
     }
 
-    public function test_not_approved()
+    public function test()
     {
         $container = $this->getContainer();
+        $em = $container->get('doctrine')->getManager();
+
         $pullRequest = new PullRequest();
         $pullRequest->setTitle('dummy');
-        $pullRequest->setApproved(false);
 
         $createUsecase = $container->get(CreatePullRequestUsecase::class);
         $createUsecase->run($pullRequest);
 
         $reviewUsecase = $container->get(ReviewPullRequestUsecase::class);
         $reviewUsecase->run($pullRequest);
-
-        $em = $container->get('doctrine')->getManager();
-        $em->clear();
-        /** @var PullRequest $req1 */
         $req1 = $em->find(PullRequest::class, 1);
         self::assertNotNull($req1);
-    }
+        self::assertEquals('fix-pr', $req1->getWorkflow()->getCurrentFlowObject()->getId(), 'go to fix-pr');
 
-    public function test_approved()
-    {
-        $container = $this->getContainer();
-        $pullRequest = new PullRequest();
-        $pullRequest->setTitle('dummy');
-        $pullRequest->setApproved(true);
 
-        $createUsecase = $container->get(CreatePullRequestUsecase::class);
-        $createUsecase->run($pullRequest);
+        $usecase = $container->get(FixPullRequestUsecase::class);
+        $usecase->run($pullRequest);
 
-        $reviewUsecase = $container->get(ReviewPullRequestUsecase::class);
-        $reviewUsecase->run($pullRequest);
-
-        $em = $container->get('doctrine')->getManager();
         $em->clear();
-        /** @var PullRequest $req1 */
         $req1 = $em->find(PullRequest::class, 1);
         self::assertNotNull($req1);
-        self::assertTrue($req1->isMerged());
+        self::assertEquals('review-pr', $req1->getWorkflow()->getCurrentFlowObject()->getId(), 'taken back to review-pr');
     }
 }
