e2599cf9060894bd4b62c9743ba5ed7e33d16f8b
77web <info@77-web.com>
2019-05-06 09:55:49 +0900

    added list view for PullRequest

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PullRequestRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var PullRequestRepository
+     */
+    private $repository;
+
+    /**
+     * @param PullRequestRepository $repository
+     */
+    public function __construct(PullRequestRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
      * @Route("/", name="index")
+     * @Template()
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
+        return [
+            'pull_reqs' => $this->repository->findBy([], ['id' => 'desc']),
+        ];
     }
 }
